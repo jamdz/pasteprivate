7055ce1bccd43102d4c36495305475bf40043cf3
jamdz <jamesiucky926@gmail.com>
2021-02-12 14:58:53 +0100

    Pasteprivate UI Design Project

---
+++ UserInterFace(UI)/assets/js/frontenderrmessage.js
@@ -0,0 +1,52 @@
+// let f;
+// let e;
+// let h;
+
+
+// f = document.getElementsByName('pasteprivate-content_title');
+// e = document.getElementsByClassName('pasteprivate_js_error');
+// h = document.getElementById('pasteprivate_content_form');
+
+// h.addEventListener('submit', () => {
+
+//     if (f == "") {
+//         e.textContent = 'Please the title field can not be left blank';
+//     } else {
+
+//     }
+
+// });
+
+let e;
+let f;
+let g;
+let gy;
+let hhy;
+let jh;
+let gf;
+let df;
+
+
+e = new Date();
+
+f = e.getDay();
+g = e.getDate();
+gy = e.getFullYear();
+
+hhy = [f, g, gy];
+
+jh = hhy[0];
+gf = hhy[1];
+df = hhy[2];
+
+
+
+
+
+
+console.log(jh, gf, df);
+
+
+
+
+// ///////////////////////////////////////////////////////////////////////////////////////////// Close moadl for success content upload  ////////////////////////////////////////////////////////////////////////////
---
+++ UserInterFace(UI)/assets/js/otherfornt.js
@@ -0,0 +1,10 @@
+let fg = document.querySelector("#privatepaste_success_close_modal");
+
+let gr = document.querySelector('.pasteprivate_success_post');
+
+
+
+
+fg.addEventListener("click", () => {
+    gr.remove();
+});
